4c7b6c88ee5f3cdda19395aac6f9ca6934aa6999
s elkhaloui <96193355+selkhaloui@users.noreply.github.com>
2022-04-22 22:45:06 +0000

    added int to code

--- pre02/DataTypes.java
+++ pre02/DataTypes.java
@@ -1,1 +1,11 @@
-class DataTypes {}
+class DataTypes {
+public static void main(String[] args) 
+int number;  
+  number = 20;
+  int value = 30;
+  int sum;
+  sum = number + value; 
+  System.out.println("Data Types");
+
+System.out.println(number);
+}
